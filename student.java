9c867fddd0b7b419f0aaca437dad0f7818bd710f
Lyell-jasmine-bulan <lyellbulan@gmail.com>
2024-11-22 07:28:01 +0800

    student.java

--- student.java
+++ student.java
@@ -1,28 +1,32 @@
-public class student extends person {
-    private String program;
+class Student extends Person{
+    private String Program;
     private int year;
 
-    public student(String name, String Address, String program, int year){
-        super(name, Address);
-        this.program = program;
+    public Student (String Name, String Address, String Program, int year){
+        this.Name = Name;
+        this.Address = Address;
+        this.Program = Program;
         this.year = year;
     }
-
-    public void setProgram(String program){
-        this.program = program;
+    
+    public void setProgram(String Program){
+        this.Program = Program;
     }
-
+    
     public void setYear(int year){
         this.year = year;
     }
-
+    
     public String getProgram(){
-        return program;
+        return Program;
     }
-
+    
     public int getYear(){
         return year;
     }
-
     
+    @Override
+    public String toString(){
+        return "Hello I am " + Name + " I lived in " + Address + ". I am " + year + " year of " + Program + ". ";
+    }
 }
